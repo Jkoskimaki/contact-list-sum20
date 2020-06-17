939a4f710111fbea53e44876f5a99bdefffb74ac
jacob koskimaki <jkoskimaki@gmail.com>
2020-06-16 19:02:07 -0600

    added styles

---
+++ notes.js
@@ -0,0 +1,8 @@
+
+
+Add semantic UI package
+
+yarn add semantic-ui-react
+yarn add semantic-ui-CSS
+
+
--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Header } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Clean up to be higher order (which is usually a logical) component
+
+class App extends Component {
+  //Simulates database, when not built out
+  state = { contacts: [
+    { id: 1, firstName: 'John', phone: '123-123-1244'},
+    { id: 2, firstName: 'Sally', phone: '133-123-1244'},
+    { id: 3, firstName: 'Monty', phone: '323-123-1244'}
+
+  ]}
+
+  render() {
+    return(
+     <div>
+       <Header size = "Huge" color = "blue" textAlign = 'center'>
+        Contact List
+       </Header>
+     </div> 
+    )
+  }
 }
 
 export default App;
